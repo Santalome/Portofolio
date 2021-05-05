cf5a6ffda923f44b90c16440b43b33a755aedfc0
Mateo Gomez <mateo.gomez.santa@gmail.com>
2021-05-04 20:02:47 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 
-
-
 //Contact form
 
-emailjs.init('service_hqx5afa');
+function sendMail(){
+    var tempParams ={
+        from_name: document.getElementById("fname").value,
+        message: document.getElementById("subject").value
+    };
+
+    emailjs.send('service_hqx5afa','template_cek8viq', tempParams)
+    .then(function(res){
+        console.log("success", res.status);
+    })
+}
